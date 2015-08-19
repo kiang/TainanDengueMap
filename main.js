56cbd4e8ca92033d67dbee81207d40d26f772a7d
Finjon Kiang <kiange@gmail.com>
2015-08-20 01:18:55 +0800

    get map works

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 $.ajaxSetup({async: false});
 
 var map;
-var week_index = 48
 $.getJSON('DengueTN.json', function (data) {
     DengueTN = data
 });
@@ -9,56 +8,62 @@ $.getJSON('DengueTN.json', function (data) {
 function initialize() {
     /*map setting*/
     map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 10,
-        center: {lat: 23.10, lng: 120.50}
+        zoom: 12,
+        center: {lat: 23.00, lng: 120.30}
     });
 
     $.getJSON('cunliTN.json', function (data) {
-        town = map.data.addGeoJson(data);
+        cunli = map.data.addGeoJson(data);
     });
-//
-//    town.forEach(function (value) {
-//        var key = value.getProperty('TOWNNAME');
-//        value.setProperty('num', DengueTN[key][week_index]);
-//    });
-//
-//    map.data.setStyle(function (feature) {
-//        var num = feature.getProperty('num');
-//        color = ColorBar(num);
-//        return {
-//            fillColor: color,
-//            fillOpacity: 0.6,
-//            strokeColor: 'gray',
-//            strokeWeight: 1
-//        }
-//    });
-//
-//    map.data.addListener('mouseover', function (event) {
-//        var Cunli = event.feature.getProperty('TOWNNAME');
-//        map.data.revertStyle();
-//        map.data.overrideStyle(event.feature, {fillColor: 'white'});
-//        $('#detial > #content').empty();
-//        $('#detial > #content').append('<div>' + Cunli + ':' + DengueTN[Cunli][week_index] + '</div>');
-//    });
-//
-//    map.data.addListener('mouseout', function (event) {
-//        map.data.revertStyle();
-//        $('#detial > #content').empty();
-//    });
-//
-//    map.data.addListener('click', function (event) {
-//        var Cunli = event.feature.getProperty('TOWNNAME');
-//        if ($('#myTab a[name|="' + Cunli + '"]').tab('show').length == 0) {
-//            $('#myTab').append('<li><a name="' + Cunli + '" href="#' + Cunli + '" data-toggle="tab">' + Cunli +
-//                    '<button class="close" onclick="closeTab(this.parentNode)">×</button></a></li>');
-//            $('#myTabContent').append('<div class="tab-pane fade" id="' + Cunli + '"><div></div></div>');
-//            $('#myTab a:last').tab('show');
-//            createStockChart(Cunli);
-//            $('#myTab li a:last').click(function (e) {
-//                $(window).trigger('resize');
-//            });
-//        }
-//    });
+
+    cunli.forEach(function (value) {
+        var key = value.getProperty('T_Name') + value.getProperty('V_Name');
+        var count = 0;
+        if(DengueTN[key]) {
+            DengueTN[key].forEach(function(val) {
+                count += val[1];
+            });
+        }
+        value.setProperty('num', count);
+    });
+
+    map.data.setStyle(function (feature) {
+        var num = feature.getProperty('num');
+        color = ColorBar(num);
+        return {
+            fillColor: color,
+            fillOpacity: 0.6,
+            strokeColor: 'gray',
+            strokeWeight: 1
+        }
+    });
+
+    map.data.addListener('mouseover', function (event) {
+        var Cunli = event.feature.getProperty('T_Name') + event.feature.getProperty('V_Name');
+        map.data.revertStyle();
+        map.data.overrideStyle(event.feature, {fillColor: 'white'});
+        $('#detial > #content').empty();
+        $('#detial > #content').append('<div>' + Cunli + ':' + event.feature.getProperty('num') + '</div>');
+    });
+
+    map.data.addListener('mouseout', function (event) {
+        map.data.revertStyle();
+        $('#detial > #content').empty();
+    });
+
+    map.data.addListener('click', function (event) {
+        var Cunli = event.feature.getProperty('T_Name') + event.feature.getProperty('V_Name');
+        if ($('#myTab a[name|="' + Cunli + '"]').tab('show').length == 0) {
+            $('#myTab').append('<li><a name="' + Cunli + '" href="#' + Cunli + '" data-toggle="tab">' + Cunli +
+                    '<button class="close" onclick="closeTab(this.parentNode)">×</button></a></li>');
+            $('#myTabContent').append('<div class="tab-pane fade" id="' + Cunli + '"><div></div></div>');
+            $('#myTab a:last').tab('show');
+            createStockChart(Cunli);
+            $('#myTab li a:last').click(function (e) {
+                $(window).trigger('resize');
+            });
+        }
+    });
     createStockChart('total');
 }
 
