e57c272ca46c74bee13f934fbd91ba0ded286f99
Finjon Kiang <kiange@gmail.com>
2015-08-31 16:16:51 +0800

    push 0830 data

--- scripts/query.php
+++ scripts/query.php
@@ -3,11 +3,11 @@
 $json = json_decode(file_get_contents(dirname(__DIR__) . '/DengueTN.json'), true);
 
 /*
- * looking for 08-29 record
+ * looking for 08-30 record
  */
 foreach ($json['total'] AS $day) {
-    if ($day[0] === '2015-08-29') {
-        die('2015-08-29 record existed');
+    if ($day[0] === '2015-08-30') {
+        die('2015-08-30 record existed');
     }
 }
 
@@ -30,14 +30,14 @@ while ($line = fgetcsv($fh, 2048)) {
             $json[$areaKey] = array();
         }
         $json[$areaKey][] = array(
-            '2015-08-29',
+            '2015-08-30',
             $line[7],
         );
     }
 }
 
 $json['total'][] = array(
-    '2015-08-29',
+    '2015-08-30',
     $total,
 );
 
