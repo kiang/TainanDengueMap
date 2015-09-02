0f96d24da25f53dace13c9bec39cbf4c128fc564
Finjon Kiang <kiange@gmail.com>
2015-09-02 23:19:22 +0800

    get age_sum.csv

--- cdc/age_sum.php
+++ cdc/age_sum.php
@@ -4,8 +4,6 @@ $fh = fopen(__DIR__ . '/Dengue_Daily.csv', 'r');
 $data = array();
 fgetcsv($fh, 2048);
 while ($line = fgetcsv($fh, 2048)) {
-    if (empty($line[7]))
-        continue;
     foreach ($line AS $k => $v) {
         $line[$k] = str_replace(array('　', ' '), '', $v);
     }
@@ -29,8 +27,39 @@ while ($line = fgetcsv($fh, 2048)) {
     }
 }
 
-foreach ($data AS $y => $r) {
-    arsort($data[$y]);
-}
+$base = array(
+    '70+' => 0,
+    '65-69' => 0,
+    '60-64' => 0,
+    '55-59' => 0,
+    '50-54' => 0,
+    '45-49' => 0,
+    '40-44' => 0,
+    '35-39' => 0,
+    '30-34' => 0,
+    '25-29' => 0,
+    '20-24' => 0,
+    '15-19' => 0,
+    '10-14' => 0,
+    '5-9' => 0,
+    4 => 0,
+    3 => 0,
+    2 => 0,
+    1 => 0,
+    0 => 0,
+);
 
-print_r($data);
+$fh = fopen(__DIR__ . '/age_sum.csv', 'w');
+fputcsv($fh, array_merge(array('年度'), array_keys($base)));
+
+foreach ($data AS $y => $levels) {
+    if ($y === 'total')
+        continue;
+    $new = $base;
+    foreach ($levels AS $level => $count) {
+        if (isset($new[$level])) {
+            $new[$level] = $count;
+        }
+    }
+    fputcsv($fh, array_merge(array($y), $new));
+}
