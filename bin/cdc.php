501972d9150a65023e20836d7e245b5124584159
auto commit <noreply@localhost>
2015-09-09 13:18:07 +0800

    auto update @ 2015-09-09 13:17:25

--- bin/cdc.php
+++ bin/cdc.php
@@ -3,13 +3,13 @@
 $rootPath = dirname(__DIR__);
 $now = date('Y-m-d H:i:s');
 
-system("{$rootPath}/cdc/query.php");
-system("{$rootPath}/cdc/points.php");
-system("{$rootPath}/cdc/age_sum.php");
-system("{$rootPath}/cdc/cunli_rate.php");
+exec("/usr/bin/php -q {$rootPath}/cdc/query.php");
+exec("/usr/bin/php -q {$rootPath}/cdc/points.php");
+exec("/usr/bin/php -q {$rootPath}/cdc/age_sum.php");
+exec("/usr/bin/php -q {$rootPath}/cdc/cunli_rate.php");
 
-system("cd {$rootPath} && /usr/bin/git add -A");
+exec("cd {$rootPath} && /usr/bin/git add -A");
 
-system("cd {$rootPath} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
+exec("cd {$rootPath} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
 
-system("cd {$rootPath} && /usr/bin/git push origin master");
+exec("cd {$rootPath} && /usr/bin/git push origin gh-pages");
