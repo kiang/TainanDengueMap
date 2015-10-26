0f6479a4588847eba85a3d87a837083dcb4dc2f0
kiang <kiange@gmail.com>
2015-10-26 14:25:38 +0800

    fix cunli code of chemical overlays

--- chemical/query.php
+++ chemical/query.php
@@ -10,6 +10,27 @@ while ($line = fgetcsv($fh, 2048)) {
 }
 fclose($fh);
 
+$mappings = array(
+    '新化區清水/觀音里' => array(
+        '6701800-006', '6701800-008'
+    ),
+    '新化區武安市場里' => array(
+        '6701800-001',
+    ),
+    '麻豆區晉江里' => array(
+        '6700700-004'
+    ),
+    '麻豆區第五市場里' => array(
+        '6700700-011'
+    ),
+    '永康區鹽行里' => array('6703100-010'),
+    '永康區鹽洲里' => array('6703100-029'),
+    '安南區沙崙里' => array('6703500-033'),
+    '北區北區里' => array(),
+    '麻豆區麻豆里' => array(),
+    '新市區三合里' => array('6702000-006'),
+);
+
 /*
  * data.tainan.gov.tw/dataset/4c260d97-e268-4b4a-8b15-c0fc92a25120/resource/f438f96e-b91c-4891-82fb-37d0b6cf71c8/download/10408.csv
  */
@@ -20,7 +41,7 @@ if (!file_exists($rawPath)) {
 $urlBase = 'http://data.tainan.gov.tw/dataset/4c260d97-e268-4b4a-8b15-c0fc92a25120/resource';
 $resources = array();
 $dataset = json_decode(file_get_contents('http://data.tainan.gov.tw/api/action/package_show?id=4c260d97-e268-4b4a-8b15-c0fc92a25120'), true);
-foreach($dataset['result']['resources'] AS $resource) {
+foreach ($dataset['result']['resources'] AS $resource) {
     $parts = explode('年', substr($resource['name'], 0, strpos($resource['name'], '月')));
     $parts[0] += 1911;
     $key = $parts[0] . str_pad($parts[1], 2, '0', STR_PAD_LEFT);
@@ -69,10 +90,20 @@ foreach ($resources AS $ym => $uuid) {
         /*
          * get cunli code
          */
+        $areaParts = explode('區', $line[2]);
         $cunliParts = explode('里', $line[3]);
+        $line[2] = $areaParts[0] . '區';
         $line[3] = $cunliParts[0] . '里';
-        if (isset($cunliCodes[$line[2] . $line[3]])) {
-            $data[$lineDayTime]['cunlis'][] = $cunliCodes[$line[2] . $line[3]];
+        $line[3] = preg_replace('/[a-z\\(\\) ]/i', '', $line[3]);
+        $finalKey = $line[2] . $line[3];
+        if (isset($mappings[$finalKey])) {
+            foreach ($mappings[$finalKey] AS $mapedCode) {
+                $data[$lineDayTime]['cunlis'][] = $mapedCode;
+            }
+        } else if (isset($cunliCodes[$finalKey])) {
+            $data[$lineDayTime]['cunlis'][] = $cunliCodes[$finalKey];
+        } else {
+            echo "{$line[2]}{$line[3]}\n";
         }
     }
     fclose($fh);
